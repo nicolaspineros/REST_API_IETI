ef7439f6890b7460b04dfaf62b236bb15fb97c73
Nicolas Piñeros Campo <onpc1224@gmail.com>
2022-08-30 22:36:09 -0500

    Implementacion HashMap

--- src/main/java/co/edu/escuelaing/REST_API_IETI/service/UserServiceHashMap.java
+++ src/main/java/co/edu/escuelaing/REST_API_IETI/service/UserServiceHashMap.java
@@ -3,6 +3,7 @@ package co.edu.escuelaing.REST_API_IETI.service;
 import co.edu.escuelaing.REST_API_IETI.entities.User;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -12,26 +13,26 @@ public class UserServiceHashMap implements UserService{
 
     @Override
     public User create(User user) {
-        return null;
+        return users.put(user.getId(), user);
     }
 
     @Override
     public User findById(String id) {
-        return null;
+        return users.get(id);
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        return new ArrayList<User>(users.values());
     }
 
     @Override
     public void deleteById(String id) {
-
+        users.remove(id);
     }
 
     @Override
     public User update(User user, String userId) {
-        return null;
+        return users.put(userId,user);
     }
 }
