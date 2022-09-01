b641586e1e38060a30f023dbac717a190c2b1318
Nicolas Piñeros Campo <onpc1224@gmail.com>
2022-08-31 23:37:41 -0500

    Pruebas

---
+++ README.md
@@ -0,0 +1,27 @@
+## API REST con Spring Boot - Microservicios - IETI
+
+## Nicolas Piñeros Campo
+
+### Crear Usuario 
+
+![](/src/test/POST-user.png)
+
+### Consultar Usuarios
+
+![](/src/test/GET-user.png)
+
+### Consultar Usuario por ID
+
+![](/src/test/GET-userbyid.png)
+
+### Actualizar Usuario por ID
+
+![](/src/test/PUT-user.png)
+
+### Eliminiar Usuario por ID
+
+![](/src/test/DELETE-user.png)
+
+### Confirmar Usuario Eliminado
+
+![](/src/test/CHECKDELETE.png)
--- src/main/java/co/edu/escuelaing/REST_API_IETI/controller/UserController.java
+++ src/main/java/co/edu/escuelaing/REST_API_IETI/controller/UserController.java
@@ -3,6 +3,7 @@ package co.edu.escuelaing.REST_API_IETI.controller;
 import co.edu.escuelaing.REST_API_IETI.dto.UserDto;
 import co.edu.escuelaing.REST_API_IETI.entities.User;
 import co.edu.escuelaing.REST_API_IETI.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +11,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
 
-    public UserController(UserService userService) {
+    public UserController(@Autowired UserService userService) {
         this.userService = userService;
     }
 
